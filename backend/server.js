e312ea3bb397714c005a7f1dd356acd46d81610f
Chen Lay <maungmaungyoker.mmy@gmail.com>
2022-01-18 20:11:04 +0630

    Removed esm package beacause of deprecated issues with morgan :pushpin:

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('Setting up server...')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+
+// create express app
+const app = express()
+
+// apply middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('common'))
+
+// routes
+app.get('/', (req, res) => res.send('You hit server endpoint...'))
+
+// port
+const port = process.env.PORT || 8000
+
+// start server
+app.listen(port, () => console.log(`Server started on port ${port}`))

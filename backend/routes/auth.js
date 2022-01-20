7bca1ae9a204d205019674f30e61e30b8c47174a
Chen Lay <maungmaungyoker.mmy@gmail.com>
2022-01-20 18:17:33 +0630

    status: Changed Folder Name to Server and Client :twisted_rightwards_arrows:

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-// controllers
-const { register } = require('../controllers/auth')
-
-router.get('/register', register)
-
-module.exports = router
--- frontend/pages/index.js
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@components/MainLayout";
-
-export default function Home() {
-	return (
-		<MainLayout>
-			<h3>Hello, World!</h3>
-		</MainLayout>
-	)
-}
